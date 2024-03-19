5655910627b1c64694dae6d966f2188c0604799a
martinamicaela frutos <martinamicaela924@gmail.com>
2024-03-19 13:34:17 +0000

    add post and style model to admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Style(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    image= models.CharField(max_length=200)
+    decor_style= models.ForeignKey(Style, on_delete=models.CASCADE, related_name="post_style")
