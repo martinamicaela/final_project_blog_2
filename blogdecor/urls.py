dabfc4536337ab3f9d398b8d78aa1a6ddf91a421
martinamicaela frutos <martinamicaela924@gmail.com>
2024-03-19 18:17:56 +0000

    create post_html to blog template

--- blogdecor/urls.py
+++ blogdecor/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_views.index, name='index'),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
