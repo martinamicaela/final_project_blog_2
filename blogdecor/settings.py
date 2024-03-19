e234826ed9f7e21d43e9a0c7d21e0a7bd0a7ede9
martinamicaela frutos <martinamicaela924@gmail.com>
2024-03-19 11:28:34 +0000

    add inf settings

--- blogdecor/settings.py
+++ blogdecor/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5m4lo010(#ukx6v#kb+%o60x&#^5$om#z#$nec3roe_x1nj+rv'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
